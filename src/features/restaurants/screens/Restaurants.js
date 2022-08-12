efae6fe01dbb94ff6e57fab9854dfdba58cd81b3
Dani Dominguez <dani.d.zamorano@gmail.com>
2022-08-12 13:52:44 +0200

    Fix ios error

--- src/features/restaurants/screens/Restaurants.js
+++ src/features/restaurants/screens/Restaurants.js
@@ -5,7 +5,7 @@ import { RestaurantInfoCard } from '../components/RestaurantInfoCard';
 
 const SafeArea = styled(SafeAreaView)`
   flex: 1;
-  margin-top: ${StatusBar.currentHeight}px;
+  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
 `;
 
 const SearchContainer = styled.View`
