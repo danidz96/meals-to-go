a77c0e3903aed30a22775f7cbbd50a5abe57e829
Dani Dominguez <dani.d.zamorano@gmail.com>
2022-08-12 13:24:16 +0200

    Create restaurant info card

--- App.js
+++ App.js
@@ -1,43 +1,11 @@
-import { useState } from 'react';
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
-import { StatusBar, StyleSheet, SafeAreaView, Text, View } from 'react-native';
-import { Searchbar } from 'react-native-paper';
+import { RestaurantsScreen } from './src/features/restaurants/screens/Restaurants';
 
 export default function App() {
-  const [searchQuery, setSearchQuery] = useState('');
-
-  const onChangeSearch = (query) => setSearchQuery(query);
-
   return (
     <>
-      <SafeAreaView style={styles.container}>
-        <View style={styles.search}>
-          <Searchbar
-            placeholder="Search"
-            onChangeText={onChangeSearch}
-            value={searchQuery}
-          />
-        </View>
-        <View style={styles.list}>
-          <Text>list</Text>
-        </View>
-      </SafeAreaView>
+      <RestaurantsScreen />
       <ExpoStatusBar style="auto" />
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight,
-  },
-  search: {
-    padding: 16,
-  },
-  list: {
-    flex: 1,
-    padding: 16,
-    backgroundColor: 'blue',
-  },
-});
