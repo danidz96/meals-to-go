093fa6c8dcc32c95fb008dec9d3d283b277f874f
Dani Dominguez <dani.d.zamorano@gmail.com>
2022-08-15 13:28:37 +0200

    Install and use google fonts

--- App.js
+++ App.js
@@ -2,8 +2,19 @@ import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 import { RestaurantsScreen } from './src/features/restaurants/screens/Restaurants';
 import { ThemeProvider } from 'styled-components/native';
 import { theme } from './src/theme';
+import { useFonts, Oswald_400Regular } from '@expo-google-fonts/oswald';
+import { Lato_400Regular } from '@expo-google-fonts/lato';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Oswald_400Regular,
+    Lato_400Regular,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <>
       <ThemeProvider theme={theme}>
